81e9e9382a53845b016910cad733939b3a664259
jubaaitidir <jubaaitidir@gmail.com>
2019-06-24 16:18:09 +0200

    Calculator.php

---
+++ src/Calculator.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_type=1);
+namespace App\Service;
+
+class Calculator
+{
+	public function add(int $firstNumber, int $secondNumber):int
+	{
+		return ($firstNumber + $secondNumber + 5);
+	}
+
+}
+
+?>
