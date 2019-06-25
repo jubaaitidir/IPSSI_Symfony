e4730f4f98083840c8a11ec0f5d581d265d0caa2
jubaaitidir <jubaaitidir@gmail.com>
2019-06-25 09:57:06 +0200

    Calculator.php

--- src/Service/Calculator.php
+++ src/Service/Calculator.php
@@ -5,11 +5,11 @@ namespace App\Service;
 
 class Calculator
 {
-	public function add(int $firstNumber, int $secondNumber):int
+	public function add(int $firstNumber, int $secondNumber): int
 	{
-		return ($firstNumber + $secondNumber + 5);
+		return ($firstNumber + $secondNumber);
 	}
 
 }
 
-?>
+
