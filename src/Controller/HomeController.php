2ef08d127bacb1edd8591613ee55449b8b2cdaae
jubaaitidir <jubaaitidir@gmail.com>
2019-06-25 11:08:38 +0200

    composer.phar

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+
+
+declare(strict_types=1);
+
+
+
+namespace App\Controller;
+
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+
+
+class HomeController
+
+{
+
+    public function handle(): Response
+
+    {
+
+        return new Response('ok');
+
+    }
+
+}
